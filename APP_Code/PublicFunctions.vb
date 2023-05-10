37048463cc9fe931ca6bd210a6e932db9e5a6708
Muhammad Hesham <eng.mohammed.hisham@gmail.com>
2023-05-11 01:43:18 +0300

    add a function to check if code is unique or not

--- APP_Code/PublicFunctions.vb
+++ APP_Code/PublicFunctions.vb
@@ -5,7 +5,6 @@ Imports System.Data.SqlClient
 Imports System.Security.Cryptography
 Imports System.IO
 Imports System.Globalization
-Imports clsMessages
 #End Region
 
 Public Class PublicFunctions
@@ -731,6 +730,36 @@ Optional ByVal MinNumber As Integer = 0) As Integer
         Catch ex As Exception
 
             Return String.Empty
+        End Try
+    End Function
+
+    ' check if code is unique or not
+    Public Shared Function IsCodeUnique(table_name As String, column_name As String, id As Integer, code As String, school_id As Integer) As Boolean
+        Try
+            Dim query As String = "select ID from " & table_name & " where " & column_name & " = '" & code & "' and " &
+                "ID <> " & id & " and isnull(IsDeleted, 0) = 0 and SchoolID = " & school_id & ";"
+            Dim dt As DataTable = DBContext.Getdatatable(query)
+            If dt.Rows.Count > 0 Then
+                Return False
+            End If
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+    ' check if code is unique or not
+    Public Shared Function IsCodeUnique(table_name As String, column_name As String, id As Integer, code As String, school_id As Integer, _sqlconn As SqlConnection, _sqltrans As SqlTransaction) As Boolean
+        Try
+            Dim query As String = "select ID from " & table_name & " where " & column_name & " = '" & code & "' and " &
+                "ID <> " & id & " and isnull(IsDeleted, 0) = 0 and SchoolID = " & school_id & ";"
+            Dim dt As DataTable = DBContext.GetdatatableTrans(query, _sqlconn, _sqltrans)
+            If dt.Rows.Count > 0 Then
+                Return False
+            End If
+            Return True
+        Catch ex As Exception
+            Return False
         End Try
     End Function
 
